05c32cbcb087406baaacd21a3a489a36ad51d4b3
Shubham Yadav <127290441+Shubham-exe@users.noreply.github.com>
2025-06-21 13:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-async function getWeather() {
-  const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=439d4b804bc8187953eb36d2a8c26a02");
-  const data = await response.json();
-  const weather = data.weather[0].main;
-  document.getElementById("weatherData").innerText = `Current weather in London: ${weather}`;
+function getWeather() {
+  document.getElementById("weatherData").innerText = "Current weather in London: Clouds";
 }
